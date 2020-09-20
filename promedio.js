19531ec14e5492b1fa6efe2abe5be5ad5e151427
vquintanilla <valeria.quintanilla.carrera@gmail.com>
2020-09-19 22:48:21 -0500

    corrección promedio

--- promedio.js
+++ promedio.js
@@ -8,52 +8,51 @@ class Maestro {
       this.materia = materia;
     }
   }
-  class MaestroFisica extends Maestro {
-    constructor(nombre, materia, promedio, antiguedad) {
-      super(nombre, materia);
-      this.promedio = promedio;
-      this.antiguedad = antiguedad + " años trabajando en esta organizacion.";
+class MaestroFisica extends Maestro {
+    constructor(nombre, materia, arreglo, antiguedad) {
+        super(nombre, materia);
+        this.arreglo = arreglo;
+        this.antiguedad = antiguedad + " años trabajando en esta organizacion.";
     }
-    //arreglo = [100, 90, 80, 50];
-  
     getPromedio() {
-        let arreglo = [100, 90, 80, 50];
-        var suma = 0;
+         var suma = 0;
 
-        for(var x = 0; x < arreglo.length; x++){
-          suma += arreglo[x];
-        }
-        var promedio = suma / arreglo.length;
-        console.log("promedio: "+promedio);
-    }
+         for(var x = 0; x < arreglo.length; x++){
+           suma += arreglo[x];
+         }
+           var promedio = suma / arreglo.length;
+           return promedio;
+        // console.log("promedio: "+promedio);
+     }
   }
   
   class MaestroMusica extends Maestro {
-    constructor(nombre, materia, promedio, edad) {
+    constructor(nombre, materia, arreglo, edad) {
       super(nombre, materia);
       this.edad = edad + " años de edad.";
-      this.promedio = promedio;
+      this.arreglo = arreglo;
     }
-    getPromedio() {
-        let arreglo = [100, 90, 80, 90];
+      getPromedio() {
+       // let arreglo = [100, 90, 80, 90];
         var suma = 0;
 
         for(var x = 0; x < arreglo.length; x++){
           suma += arreglo[x];
         }
-        var promedio = suma / arreglo.length;
-        console.log("promedio: "+promedio);
+          var promedio = suma / arreglo.length;
+          return promedio;
+       // console.log("promedio: "+promedio);
     }
   }
-  promedio = [100, 90, 80, 95];
+  arreglo = [100, 90, 80, 95];
   
-  const Pedro = new MaestroFisica("Pedro", "Fisica", promedio, 15);
+  const Pedro = new MaestroFisica("Pedro", "Fisica", arreglo, 15);
   console.log(Pedro);
-  console.log(Pedro.getPromedio());
+  console.log("promedio: "+Pedro.getPromedio());
   console.log(Pedro.antiguedad);
   
-  const Mauricio = new MaestroMusica("Mauricio", "Musica", promedio, 35);
+  const Mauricio = new MaestroMusica("Mauricio", "Musica", arreglo, 35);
   console.log(Mauricio);
-  console.log(Mauricio.getPromedio());
+  console.log("promedio: "+Mauricio.getPromedio());
   console.log(Mauricio.edad);
   
